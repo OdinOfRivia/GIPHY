cd223df8dfeb79344c28ea74c623c91cf7b36610
MathiasMendoza <mathiasmendoza_vargas14@hotmail.com>
2023-03-01 12:19:33 -0800

    API code optimized

--- src/main.js
+++ src/main.js
@@ -1,29 +1,32 @@
-// Cache the query selector calls
+// Cache the query selector calls to improve performance
 const menuBtn = document.querySelector(".menu-btn");
 const burgerLine = document.querySelector(".menu-btn-burger");
 const nav = document.querySelector(".nav");
 
+// Define a function to toggle the menu on click
 const toggleMenu = () => {
   burgerLine.classList.toggle("open");
   nav.classList.toggle("open");
 };
 
+// Attach the toggleMenu function to the click event of the menu button
 menuBtn.addEventListener("click", toggleMenu);
 
-// Define the API URLs and request parameters using const
+// Define the API URLs and request parameters using const to ensure they are not reassigned
 const searchUrl = "https://api.giphy.com/v1/gifs/search";
 const trendUrl = "https://api.giphy.com/v1/gifs/trending";
-const key = "?api_key=EpaFLvbdU1y8QN2BH18EPGe86kSg8S77";
+const key = "?api_key=NU4sW44isKZGQFbQjDanji7HIM4XYkpK";
 const limit = "&limit=5";
 const offset = "&offset=0";
 const rating = "&rating=g";
 const language = "&lang=en";
 
-// Use template literals to build the URL
+// Use template literals to build the URL for the trending API
 const trendApi = `${trendUrl}${key}${limit}${rating}${language}`;
 
-// Use arrow function syntax for consistency
+// Use arrow function syntax for consistency and to simplify code
 const sendHttpRequest = async (url, method, data) => {
+  // define headers only if data is present to minimize object creation
   const headers = data ? { "Content-Type": "application/json" } : {};
 
   const response = await fetch(url, {
@@ -32,25 +35,32 @@ const sendHttpRequest = async (url, method, data) => {
     body: data && JSON.stringify(data),
   });
 
-  const responseData = await response.json();
-  console.log(responseData);
-  return responseData;
+  // return the response data as JSON without logging it
+  return await response.json();
 };
 
+// Define a function to fetch the most engaging gifs each day using the trending API
 const getTrend = async () => {
-  const responseData = await sendHttpRequest(trendApi, "GET");
-  console.log(responseData);
-  return responseData;
+  // send an HTTP GET request to the trending API using the sendHttpRequest function
+  return sendHttpRequest(trendApi, "GET");
 };
 
+// Define a function to search for gifs based on a keyword
 const searchGif = async (keyWord) => {
+  // use template literals to build the URL for the search API
   const url = `${searchUrl}${key}&q=${keyWord}${limit}${offset}${rating}${language}`;
-  const responseData = await sendHttpRequest(url, "GET");
-  console.log(responseData);
-  return responseData;
+
+  // send an HTTP GET request to the search API using the sendHttpRequest function
+  return sendHttpRequest(url, "GET");
 };
 
-// Use arrow function syntax for consistency
+// Execute the getTrend function when the page is loaded using jQuery
 $(window).on("load", () => {
-  getTrend();
+  getTrend().then((responseData) => {
+    // log the response data for testing purposes
+    console.log(responseData);
+  }).catch((error) => {
+    // handle any errors that occur during the HTTP request
+    console.error(error);
+  });
 });
