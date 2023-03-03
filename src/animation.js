ce5529ed25dc3a630ec714b233da7edaf62b797a
OdinOfRivia <gyunay@hotmail.dk>
2023-03-03 11:17:00 -0800

    FInished

--- src/animation.js
+++ src/animation.js
@@ -1,21 +1,37 @@
-// all the animation here...
 
-const grid = document.querySelectorAll(".grid");
-console.log();
+// gsap.from(".an1", { duration: 1,opacity:0 ,x: "-100%", ease: "steps (100)", stager: .5 });
+// gsap.from(".an2", { duration: 1,opacity:0 ,x: "100%", ease: "steps (100)", stager: .5 });
+// const timeline = gsap.timeline({defaults: {duration: 1}})
 
-// Define the scroll animation
+// gsap:registerPlugin(scrollTrigger);
+
 const tl = gsap.timeline({
-	scrollTrigger: {
-		trigger: grid,
-		start: "top 80%",
-		end: "bottom 20%",
-		scrub: true,
-	},
-});
+    defaults: { duration: 1, opacity: 0, x: "-100%", ease:"steps(100)", stager: .5 },
+    scrollTrigger: {
+      trigger: '.an1',
+      start: 'top 20%',
+      end: 'bottom 80%',
+      // scrub: true,
+       markers: false
+    }
+  });
+  
+  // Add animation to each item
+  tl.from('.an1', { stagger: 0.2 });
 
-// Add animation steps
-tl.fromTo(grid, { y: "-100%" }, { y: "0%", duration: 1 }).fromTo(
-	grid.children,
-	{ opacity: 0, y: 50 },
-	{ opacity: 1, y: 0, stagger: 0.1, duration: 0.5 }
-);
+
+
+  
+  const tl2 = gsap.timeline({
+    defaults: { duration: 1, opacity: 0, x: "100%", ease:"steps(100)", stager: .5 },
+    scrollTrigger: {
+      trigger: '.an2',
+      start: 'top 10%',
+      end: 'bottom 70%',
+      // scrub: true,
+      markers: false
+    }
+  });
+  
+  // Add animation to each item
+  tl2.from('.an2', { stagger: 0.2 });
